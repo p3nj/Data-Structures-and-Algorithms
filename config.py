8688e7d0c569529385e9482ba10431a4644f2a43
Penj <penguinjoints@icloud.com>
2023-05-24 21:36:30 +1000

    Tune up the default difficulty

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-
 class Config:
     PROBLEM_SIZE_FILE = "prob_sizes.bin"
     DATABASE_SIZE_FILE = "databases.bin"
-    SAMPLE_SIZE = 5
+    SAMPLE_SIZE = 10
     FIBONACCI_LEVEL = 20
     TEST_ROUND = 10
     LOGGER_NAME = "LOG"
